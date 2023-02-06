37ad43e8d4583113ff42e54e803c816c205b211d
Guilherme Tavares <guilhermet.silva@viannasempre.com.br>
2023-02-06 16:29:14 -0300

    mostrador na página

--- script.js
+++ script.js
@@ -5,16 +5,20 @@ const btenviar = document.querySelector('.enviar').addEventListener('click',addT
 const desc = document.querySelector('#desc')
 const valor = document.querySelector('#valor')
 const tipo = document.querySelector("input[name='tipo']:checked").value
+const containerGastos = document.querySelector('.containerGastos')
+const contadorTransacoes = document.querySelector('.contadorTransacoes')
 let valorEntrada = 0
 let valorSaida = 0
 let valorTotal = 0
-
+let contadorT = 0
 
 renderMoney()
+
 function renderMoney(){
     totsaida.innerHTML = `R$ ${valorSaida}`
     totentrada.innerHTML = `R$ ${valorEntrada}`
     total.innerHTML= `R$ ${valorTotal}`
+    contadorTransacoes.innerHTML =`${contadorT}`
 }
 
 function addTransicao(e){
@@ -33,6 +37,7 @@ function addTransicao(e){
         },1000)
         return
     }
+    contadorT++
     if(tipo == 'entrada'){
         console.log(tipo)
         addEntrada()
@@ -48,24 +53,32 @@ function addEntrada(){
     valorTotal += Number(valor.value)
     valorEntrada += Number(valor.value)
     renderMoney()
-    showGastos()
+    const div = criaDiv()
+    div.classList.add("containerEntrada")
+    containerGastos.appendChild(div)
+    renderMoney()
+    limpaCampos()
 }
 function addSaida(){
     valorTotal -= Number(valor.value)
     valorSaida += Number(valor.value)
     renderMoney()
-    showGastos()
+    const div = criaDiv()
+    div.classList.add("containerSaida")
+    containerGastos.appendChild(div)
+    renderMoney()
+    limpaCampos()
 }
+
 function limpaCampos(){
     desc.value = ''
     valor.value = ''
 }
-function showGastos(){
 
-}
 
 function criaDiv(){
     const div = document.createElement('div')
+    div.innerHTML = `<h2>Valor: R$ ${valor.value}</h2><p>Descrição: ${desc.value}</p><p>Tipo: ${tipo.value}</p><p class="remove">Remove</p>`
     return div
 }
 
